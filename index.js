23698ad7e56baa05f8b7e188d6afc3147d1ff5f7
bernaimerii <bernaimeri06@gmail.com>
2024-12-12 09:25:12 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('It works'))
 
-let myUnusedVariable = 10; // Diese Variable wird deklariert, aber nie verwendet
-let variable = 20;
+// index.js
+const myUnusedVariable = 10;
 
-if (variable == 10) {  // Hier wird '==' verwendet
+if (myUnusedVariable === 10) {
   console.log('Test');
 }
