9e6579b750b119e090c366916a988bbff55a2dde
bernaimerii <bernaimeri06@gmail.com>
2024-12-12 09:27:35 +0100

    formatter test

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import chalk from 'chalk';
 
-console.log(chalk.blue('It works'))
+console.log(
+  chalk.blue(
+    'It works',
+  ),
+);
 
 // index.js
 const myUnusedVariable = 10;
 
-if (myUnusedVariable === 10) {
-  console.log('Test');
+if (
+  myUnusedVariable ===
+  10
+) {
+  console.log(
+    'Test',
+  );
 }
